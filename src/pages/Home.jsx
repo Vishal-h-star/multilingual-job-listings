866d9d8b82cdd1af0452da82abc3290041f08418
vishal-h-star <vc8368@gmail.com>
2025-12-23 19:42:55 +0530

    home Page responsive issue fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,17 +10,17 @@ export default function Home() {
   return (
     <div className="min-h-[70vh] flex items-center justify-center bg-gradient-to-br from-blue-50 via-white to-purple-50 px-6">
       <div className="max-w-4xl mx-auto text-center">
-        <div className="mb-8">
+        <div className="mb-8 mt-8">
           <span className="inline-block px-4 py-2 bg-blue-100 text-blue-700 rounded-full text-sm font-semibold mb-6">
             🌍  {t("buttons.platformTag")}
           </span>
         </div>
 
-        <h1 className="text-5xl md:text-6xl font-bold text-gray-900 mb-6">
+        <h1 className=" text-4xl  md:text-6xl font-bold text-gray-900 mb-6">
           {t("home.title")}
         </h1>
 
-        <p className="text-xl text-gray-600 mb-10 max-w-2xl mx-auto">
+        <p className=" text-[1rem]  md:text-xl text-gray-600 mb-10 max-w-2xl mx-auto">
           {t("home.homeText")}
         </p>
 
